73806280b9f3e4c82ef9ce8049aa66299c2858ad
p8s8liushim <p8s8liushim@yujing.live>
2023-12-20 19:20:58 +0800

    graphql

--- src/main/java/com/lsm1998/blog/presentation/controller/UsersController.java
+++ src/main/java/com/lsm1998/blog/presentation/controller/UsersController.java
@@ -1,11 +1,12 @@
 package com.lsm1998.blog.presentation.controller;
 
 import com.lsm1998.blog.presentation.dto.LoginDto;
+import graphql.annotations.annotationTypes.GraphQLType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@GraphQLType
 public class UsersController
 {
     @PostMapping("/login")
